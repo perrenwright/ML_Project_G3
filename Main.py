6b462c7d07eed93eefcf2b308f088be369df12a0
Perren Wright <pwright2410@gmail.com>
2018-11-23 15:22:49 -0600

    Build and printed dataset

--- Main.py
+++ Main.py
@@ -1,6 +1,14 @@
 #Perren Wright
 #Cleveland Heart Dataset
-
+#Going through the dataset
+import pandas
+import numpy
+import sys
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/heart-disease/processed.cleveland.data"
+attributes = ["age", "sex", "cp", "trestbps", "chol", "fbs", "restecg", "thalach", "exang", "oldpeak", "slope", "ca", "thal", "num"]
+dataframe = pandas.read_csv(url, names = attributes)
+array = dataframe.values
+print (array) 
 print("Clevland Heart Disease Dataset")
 
 print("banknote authentication Data Set")
